36ebf6e06b018ae3173756de71fd1de75046049c
hackscreed <rajputgr513gssm@gmail.com>
2020-10-02 13:12:13 +0530

    routes

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // import header from 'views/header.js';
 const express = require('express');
+
 const app = express();
+
 // const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const e = require('express');
@@ -16,90 +18,65 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("views/static"));
 
 
-// Code for using the mongo below. Uncomment to use instead of google sheets.
-
-// mongo configarations
-
-// const connectionString = "mongodb+srv://sampleuser:gauravDgreat@sample.gw2iw.mongodb.net/contactRequests?retryWrites=true&w=majority";
-
-// mongoose.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true}, (error, success) => {
-//     if (error){
-//         console.log(error.code + ", " + error.errmsg);
-//     }else {
-//         console.log('connected');
-//         }
-//     }
-// );
-
-// const contactSchema = new mongoose.Schema({
-//     Email: String,
-//     name: String,
-//     message: String
-// });
-
-// const userModel = new mongoose.model('userModel', contactSchema);
-//un comment the above lines for using mongo
-
-// end of mongoose config
-
-// express config
-
-// place your static folders here for serving here
-
-//app.use(express.static(path.join(__dirname, "user serving directory")))
-
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-//// Code for sheets API 
-
-
-const auth = new google.auth.GoogleAuth({
-    keyFile: './key.json',
-    scopes: ['https://www.googleapis.com/auth/spreadsheets']
-})
-
-// Read the values into the spreadsheets, The logic for fetching a particular value will be made later
 // go below for writing the data
-
-//
-// function readData(auth){
-//     const sheets = google.sheets({version: 'v4', auth: auth})
-//     sheets.spreadsheets.values.get(
-//         {
-//             spreadsheetId: '1pK1VKKGF7pGbhPzDZnh39nEDGylWZ5zxEduha1xtzIM',
-//             range: 'A1:B5'  
-//         },(err, res)=>{
-//             if (err){
-//                 return console.log(err)
-//             }
-//             else{
-//                 console.log(res)
-//             }
-//         }
-//     )
-// }
-
 
 // Writing the data into the spreadsheets
 
-let count = 1;
-function writeData(auth, arr){
-    const sheets = google.sheets({version: 'v4', auth: auth})
-    sheets.spreadsheets.values.update(
-        {
+let count = 2;
+
+let register = 2;
+
+async function writeData(arr){
+    const auth = await new google.auth.GoogleAuth({
+        keyFile: './key.json',
+        scopes: ['https://www.googleapis.com/auth/spreadsheets']
+    })
+    const sheets = await google.sheets({version: 'v4', auth: auth});
+
+    await sheets.spreadsheets.values.batchUpdate({
             spreadsheetId: '1pK1VKKGF7pGbhPzDZnh39nEDGylWZ5zxEduha1xtzIM',
-            range: `A${count}`,
-            valueInputOption: 'USER_ENTERED',
-            resource: {
-                values: arr
+
+            requestBody: {
+                data: [
+                    {
+                        range: `Sheet1!A${count}:D${count}`,
+                        majorDimension: "COLUMNS",
+                        values: arr
+                    }
+                ],
+                valueInputOption: "USER_ENTERED"
             }
-        }, update => {
-            console.log(update)
-        }
-    )
-    count += 4;
+    }
+    );
+            count += 1;
+}
+
+
+// Below is for updating the registartions data 
+
+async function registerData(arr){
+    const auth = await new google.auth.GoogleAuth({
+        keyFile: './key.json',
+        scopes: ['https://www.googleapis.com/auth/spreadsheets']
+    })
+    const sheets = await google.sheets({version: 'v4', auth: auth});
+
+    await sheets.spreadsheets.values.batchUpdate({
+            spreadsheetId: '1lIjJeKrZaM2fuv_hJIZfjbtiCBw5qjSxpfRx98Gya20',
+
+            requestBody: {
+                data: [
+                    {
+                        range: `Sheet1!A${register}:D${register}`,
+                        majorDimension: "COLUMNS",
+                        values: arr
+                    }
+                ],
+                valueInputOption: "USER_ENTERED"
+            }
+    }
+    );
+            register += 1;
 }
 
 app.use(bodyParser.urlencoded({extended: true}))
@@ -108,29 +85,24 @@ app.get("/", function(req, res){
 });
 
 app.get("/contact", (req, res)=>{
-    res.sendFile(path.join(__dirname, "/views/static/main.html"))
+    // res.sendFile(path.join(__dirname, "/views/static/main.html"))
+    res.render('contact')
 })
 
 app.post('/', (req, res) => {
     console.log(req.body);
 
-    ///////  Below code is for model of Mongoose Uncomment if u wanna use mongo instead of sheets API
-
-    // let model = new userModel({
-    //     Email: req.body.email,
-    //     name: req.body.firstname,
-    //     type: req.body.from,
-    //     message: req.body.msg
-    // })
-    // model.save(err=>{
-    //     if(err){
-    //         console.log(err)
-    //     }
-    // })
-    // console.log(model)
-
     let UserData = [[req.body.email], [req.body.firstname], [req.body.from], [req.body.msg]];
-    writeData(auth, UserData);
+    writeData(UserData);
+    res.render('index');
+});
+
+app.post('/register', (req,res)=>{
+    console.log(req.body);
+
+    let UserData = [/*  pass the request body parameters in each inside the array */];
+    registerData(UserData);
+    res.render('index');
 })
 
 app.listen(3000)
