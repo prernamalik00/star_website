7074388cd61e1991dbb8156c5b83c1302cec526a
prernamalik00 <prernamalik00@gmail.com>
2020-12-25 03:10:25 +0530

    ready for heroku

--- index.js
+++ index.js
@@ -139,6 +139,6 @@ app.get("/mentorship", (req, res) => {
   res.render("mentorship-program");
 });
 
-app.listen(3000 || process.env.PORT, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log("Server running at port 3000");
 });
