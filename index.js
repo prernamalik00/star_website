7bae5966efe8954617719282ad2729ec453ab32c
prernamalik00 <prernamalik00@gmail.com>
2020-12-25 03:14:07 +0530

    commenting console logs

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ app.get("/contact", (req, res) => {
 });
 
 app.post("/", (req, res) => {
-  console.log(req.body);
+  // console.log(req.body);
 
   let UserData = [
     [req.body.email],
@@ -97,7 +97,7 @@ app.post("/", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
-  console.log(req.body);
+  // console.log(req.body);
 
   let UserData = [
     [req.body.firstname],
